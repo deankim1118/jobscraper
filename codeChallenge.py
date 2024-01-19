faaf7732f217a9eb327ac747ca54629a9d52ef2e
Dean Kim <deankim1118@gmail.com>
2024-01-19 00:52:42 -0500

    Update codeChallenge.py

--- codeChallenge.py
+++ codeChallenge.py
@@ -2,16 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 class Scraper:
-    def __init__(self, keywords = []):
-        self.keywords = keywords
- 
+    def __init__(self):
+        self.job_lists = []
         
-    def scraping_page(self):
-        for key in self.keywords:
-            response = requests.get(f"https://remoteok.com/remote-{key}-jobs", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"})
-            soup = BeautifulSoup(response.content, "html.parser")
-            print(response.status_code)
+    def get_response(self,url):
+        response = requests.get(f"https://remoteok.com/remote-{url}-jobs", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"})
+        soup = BeautifulSoup(response.content, "html.parser")
+        return soup
+        
+    def scraping_page(self, url):
+        soup = self.get_response(url)
+        jobs = soup.find("table", id="jobsboard").find_all("td", class_="company_and_position")[1:]
 
-
-scraping = Scraper(["flutter", "ios"])
-scraping.scraping_page()
+        for job in jobs:
+            company = job.find("h3")
+            title = job.find("h2")
+            if len(job.find_all("div", class_="location")) >= 2:
+                location, *others, income = job.find_all("div", class_="location")
+            else: 
+                location = job.find("div", class_="location")
+            
+            job_data = {
+                "company": company.text.strip(),
+                "title": title.text.strip(),
+                "location": location.text,
+                "income": income.text
+            }
+            self.job_lists.append(job_data)
+            
+    def scraping_all(self, url = []):
+        for u in url:
+            self.scraping_page(u)  
+        print(self.job_lists)
+              
+scraping = Scraper()
+scraping.scraping_all(["flutter","ios","java"])
